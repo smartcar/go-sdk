0dd5169c722c0be6517d99ac78971025a091d379
Robert Simari <rsimari@nd.edu>
2026-01-07 10:15:40 -0800

    chore: deprecating Go SDK support (#27)

--- smartcar.go
+++ smartcar.go
@@ -1,6 +1,8 @@
 // Package smartcar is the official Go SDK of the Smartcar API.
 // Smartcar is the only vehicle API built for developers, by developers.
 // Learn more about Smartcar here, https://smartcar.com/
+//
+// Deprecated: This package is no longer maintained. Please migrate to an alternative solution.
 package smartcar
 
 import (
